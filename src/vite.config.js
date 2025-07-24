647418ce9c9f1d5d2f6d379957a08193cac29af7
chefsquatch <chef.leslie.fleming@gmail.com>
2025-07-24 09:48:25 -0400

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-  optimizeDeps: {
-    exclude: ['@xenova/transformers', 'pdfjs-dist']
-  },
-  build: {
-    rollupOptions: {
-      external: ['@xenova/transformers', 'pdfjs-dist']
-    }
-  }
-});
+optimizeDeps: {
+  exclude: ['@xenova/transformers', 'pdfjs-dist']
+},
+build: {
+  rollupOptions: {
+    external: ['@xenova/transformers', 'pdfjs-dist']
+}
+}
