e0dc08f351caf65bcde10b6a83ec043fe987eb45
chefsquatch <chef.leslie.fleming@gmail.com>
2025-07-24 09:39:02 -0400

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,6 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  optimizeDeps: {
+    exclude: ['@xenova/transformers']
+  },
+  build: {
+    rollupOptions: {
+      external: ['@xenova/transformers']
+    }
+  }
 });
